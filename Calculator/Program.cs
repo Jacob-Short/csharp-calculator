4d0f423a23349a0c4e893ae9eac93457fde2c247
Jacob-Short <thatmanjshort@gmail.com>
2021-11-13 13:18:51 -0800

    created calculator in c#

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Go ahead and enter a number:\n");
+            double inputNumberOne = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Now please enter your second number:\n");
+            double inputNumberTwo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Result:\n{inputNumberOne + inputNumberTwo}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
